0238dec690e0be84f59be74c40acce70b4877875
erkulow <erkulow26@gmail.com>
2023-06-22 12:52:37 +0600

    fix

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import axios from 'axios'
+
+const page = async () => {
+	const { data } = await axios.get(
+		'https://jsonplaceholder.typicode.com/posts/1',
+	)
+
+	return <div>{JSON.stringify(data)}</div>
+}
+
+export default page
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+interface signInProps {}
+
+const page: FC<signInProps> = () => {
+	return <div>Sign up</div>
+}
+
+export default page
