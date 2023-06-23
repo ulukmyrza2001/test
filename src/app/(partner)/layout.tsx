e482ea2be483fde8a3069e880d4e1130fd6a2c97
erkulow <erkulow26@gmail.com>
2023-06-23 15:07:51 +0600

    add pages

---
+++ src/app/(partner)/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'Стать партнером',
+  description: 'Стать партнером Cheber'
+}
+
+export default function PartnerLayout({ children }: { children: React.ReactNode }) {
+  return <section>{children}</section>
+}
---
+++ src/app/(partner)/partner/page.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+const Page = () => {
+  return <div>Partnasder</div>
+}
+
+export default Page
