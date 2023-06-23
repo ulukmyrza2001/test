7807f0ebd83b6369259cff61c3d497652a65f9bb
a6uxa4 <a6uxa4@icloud.com>
2023-06-23 15:47:24 +0600

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,31 @@
 'use client'
 
+import React, { useState } from 'react'
+import { LonelySelect } from '../components/UI/Select/LonelySelect'
+
 export default function Home() {
-   return <main>CHEBER</main>
+  const [state, setState] = useState(null)
+
+  const options = [
+    { value: 1, label: 'Одежда' },
+    { value: 2, label: 'Рубашки' },
+    { value: 3, label: 'Штаны' }
+  ]
+
+  console.log(state)
+  return (
+    <main>
+      <LonelySelect
+        options={options}
+        label="Категория"
+        placeholder="Категория"
+        noOptionsMessage={() => 'abu'}
+        value={state}
+        isClearable={true}
+        onChange={(e) => setState(e)}
+        isLoading={true}
+        isDisabled={false}
+      />
+    </main>
+  )
 }
