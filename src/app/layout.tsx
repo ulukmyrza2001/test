077f99528bebebd203eee0ef0d27de280ab82c68
erkulow <erkulow26@gmail.com>
2023-06-23 12:47:52 +0600

    pull

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,20 +2,16 @@ import Providers from '../components/Provieders/Provieders'
 import './globals.css'
 
 export const metadata = {
-   title: 'Cheber',
-   description: 'Welcome to the cheber',
+  title: 'Cheber',
+  description: 'Welcome to the cheber'
 }
 
-export default function RootLayout({
-   children,
-}: {
-   children: React.ReactNode
-}) {
-   return (
-      <html lang="en">
-         <body>
-            <Providers>{children}</Providers>
-         </body>
-      </html>
-   )
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  )
 }
