81e390d0508a91a2ecf9bba17bca0cc4f34a389b
a6uxa4 <a6uxa4@icloud.com>
2023-06-27 16:22:03 +0600

    save

--- src/app/[city]/page.tsx
+++ src/app/[city]/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { ContainerSlider } from '@/src/components/ContainersSliders/ContainerSlider'
 import { Metadata } from 'next'
 import styled from 'styled-components'
 
@@ -20,48 +19,5 @@ export async function generateMetadata({ params: { city } }: CityProps): Promise
 }
 
 export default function MainPage({ params: { city } }: CityProps) {
-  return (
-    <div>
-      {/* City route = {city} */}
-      <StyledContainer>
-        <ContainerSlider
-          dots={true}
-          infinite={true}
-          speed={400}
-          slidesToShow={3}
-          slidesToScroll={1}
-          autoplay={false}
-          arrowAndprev={true}
-          typeButton={true}
-        >
-          <StyledCard>
-            <span>ABU</span>
-          </StyledCard>
-          <StyledCard>
-            <span>ABU</span>
-          </StyledCard>
-          <StyledCard>
-            <span>ABU</span>
-          </StyledCard>
-          <StyledCard>
-            <span>ABU</span>
-          </StyledCard>
-          <StyledCard>
-            <span>ABU</span>
-          </StyledCard>
-        </ContainerSlider>
-      </StyledContainer>
-    </div>
-  )
+  return <div>City route = {city}</div>
 }
-
-const StyledContainer = styled.div`
-  width: 90%;
-  margin: auto;
-`
-
-const StyledCard = styled.div`
-  width: 435px;
-  height: 100px;
-  background-color: teal;
-`
--- src/components/ContainersSliders/ContainerSlider.tsx
+++ src/components/ContainersSliders/ContainerSlider.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Slider from 'react-slick'
+import NavigateNextIcon from '@mui/icons-material/NavigateNext'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
-import NavigateNextIcon from '@mui/icons-material/NavigateNext'
-import styled from 'styled-components'
+import './ContainerSlider.css'
 
 //dots - Внизу показывает на каком Slider
 //infinte - Slider работает бесконечно
@@ -20,34 +20,40 @@ import styled from 'styled-components'
 function SampleNextArrow(props: any) {
   const { onClick, typeButton } = props
   return (
-    <StyledContainerNextArrow onClick={onClick} $typeButton={typeButton}>
+    <div
+      className={typeButton ? 'slider_button_true_next' : 'slider_button_false_next'}
+      onClick={onClick}
+    >
       <NavigateNextIcon />
-    </StyledContainerNextArrow>
+    </div>
   )
 }
 
 function SamplePrevArrow(props: any) {
   const { onClick, typeButton } = props
   return (
-    <StyledContainerPrevArrow onClick={onClick} $typeButton={typeButton}>
+    <div
+      className={typeButton ? 'slider_button_true_prev' : 'slider_button_false_prev'}
+      onClick={onClick}
+    >
       <NavigateNextIcon />
-    </StyledContainerPrevArrow>
+    </div>
   )
 }
 
 function AppendDots(props: any) {
   return (
-    <StyledContainerDots>
+    <div className="container_dots">
       {props.dots.map((item: any) => {
         return (
-          <StyledContainerAppendDots
+          <div
             onClick={item.props.children.props.onClick}
             key={item.key}
-            $active={item.props.className}
+            className={item.props.className === 'slick-active' ? 'dots_true' : 'dots_false'}
           />
         )
       })}
-    </StyledContainerDots>
+    </div>
   )
 }
 
@@ -83,73 +89,8 @@ export const ContainerSlider = ({
     appendDots: (dots: any) => <AppendDots dots={dots} />
   }
   return (
-    <StyledContainerSlider>
+    <div className="container_slider">
       <Slider {...settings}>{React.Children.toArray(children)}</Slider>
-    </StyledContainerSlider>
+    </div>
   )
 }
-
-const StyledContainerSlider = styled.div`
-  width: 100%;
-  .slick-track {
-    display: flex;
-    gap: 10px;
-  }
-`
-const StyledContainerNextArrow = styled.div<{ $typeButton: boolean }>`
-  margin: 0 20px;
-  position: absolute;
-  top: ${({ $typeButton }) => ($typeButton ? '0' : '40%')};
-  left: ${({ $typeButton }) => ($typeButton ? 'calc(100% - 50px)' : 'calc(100% - 15px)')};
-  transform: ${({ $typeButton }) => ($typeButton ? '' : 'translateY(-50%)')};
-  width: 28px;
-  height: 28px;
-  border: 1px solid #acacac;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  border-radius: 100%;
-  cursor: pointer;
-  &:hover {
-    border: 1.5px solid #acacac;
-    box-shadow: 0px 0px 4px 1px #acacac;
-  }
-`
-const StyledContainerPrevArrow = styled.div<{ $typeButton: boolean }>`
-  margin: ${({ $typeButton }) => ($typeButton ? '5px -20px' : '0')};
-  position: ${({ $typeButton }) => ($typeButton ? 'relative' : 'absolute')};
-  top: ${({ $typeButton }) => ($typeButton ? '0' : '40%')};
-  left: ${({ $typeButton }) => ($typeButton ? 'calc(100% - 50px)' : '-35px')};
-  transform: ${({ $typeButton }) =>
-    $typeButton ? 'rotate(180deg)' : 'translateY(-50%) rotate(180deg);'};
-  width: 28px;
-  height: 28px;
-  border: 1px solid #acacac;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  border-radius: 100%;
-  cursor: pointer;
-  &:hover {
-    border: 1.5px solid #acacac;
-    box-shadow: 0px 0px 4px 1px #acacac;
-  }
-`
-const StyledContainerDots = styled.div`
-  width: 100%;
-  height: 30px;
-  display: flex;
-  align-items: end;
-  justify-content: center;
-  gap: 5px;
-  cursor: pointer;
-`
-const StyledContainerAppendDots = styled.div<{ $active: string }>`
-  width: 6px;
-  height: ${({ $active }) => ($active === 'slick-active' ? '26px' : '16px')};
-  background-color: ${({ $active }) => ($active === 'slick-active' ? 'gray' : '#acacac')};
-  border-radius: 20px;
-  transition: 300ms;
-`
