2169cb50bde1503f226778393ab5a315dbc72d9d
erkulow <erkulow26@gmail.com>
2023-06-27 11:41:57 +0600

    pull

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,39 @@
 'use client'
+import React from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import styles from './Login.module.css'
+
+interface FormData {
+  numberField: number
+  passwordField: string
+}
 
 export default function Login() {
-  return <div>Login</div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>()
+
+  const onSubmit: SubmitHandler<FormData> = (data: any) => {
+    console.log(data)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+      <div>
+        <label>Number Field:</label>
+        <input type="number" {...register('numberField', { required: true })} />
+        {errors.numberField && <span>This field is required</span>}
+      </div>
+
+      <div>
+        <label>Password Field:</label>
+        <input type="password" {...register('passwordField', { required: true })} />
+        {errors.passwordField && <span>This field is required</span>}
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+  )
 }
