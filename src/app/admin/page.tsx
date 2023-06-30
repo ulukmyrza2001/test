3bc87b938ebb297ca9c84c5a12a11f12ff98e29c
a6uxa4 <a6uxa4@icloud.com>
2023-06-30 19:30:31 +0600

    save

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,12 +2,10 @@
 
 import { Container } from '@/src/styles/ContainerStyle/Container'
 import AdminLayout from './layout'
-import { Dashboard } from '@/src/components/Dashboard/Dashboard'
 
 export default function Admin() {
   return (
     <AdminLayout>
-      <Dashboard />
       <Container>Admin</Container>
     </AdminLayout>
   )
