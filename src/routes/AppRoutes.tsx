464bc27dfb79228f867a4753c814e95fc7ddd896
a6uxa4 <a6uxa4@icloud.com>
2023-07-03 19:28:55 +0600

    save

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,16 +3,15 @@ import { SuperAdmin } from './SuperAdmin'
 import { UserRoutes } from './UserRoutes'
 
 export const AppRoutes = () => {
-	const role = 'USER'
+	const role = 'ADMIN'
 
 	switch (role) {
 		// case 'SUPER_ADMIN':
 		// 	return <SuperAdmin />
-		// case 'ADMIN':
-		// 	return <AdminRoutes />
-		case 'USER':
-			return <UserRoutes />
-
+		case 'ADMIN':
+			return <AdminRoutes />
+		// case 'USER':
+		// 	return <UserRoutes />
 		default:
 			return <UserRoutes />
 	}
