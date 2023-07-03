c388fdbde323f172a636c7d56986e35b230e202e
a6uxa4 <a6uxa4@icloud.com>
2023-07-03 20:00:10 +0600

    save

--- src/routes/AdminRoutes.tsx
+++ src/routes/AdminRoutes.tsx
@@ -1,15 +1,22 @@
 import { Route, Routes } from 'react-router'
 import { AdminOutlet } from '../layout/admin-outlet/Dashboard'
-import { AdminPage } from '../pages/admin'
 import { ADMIN_ROUTES } from '../utils/constants/routes'
+import { Calendar } from '../pages/admin/Calendar/Calendar'
+import { Master } from '../pages/admin/Master/Master'
+import { Services } from '../pages/admin/Services/Services'
 
 export const AdminRoutes = () => {
 	return (
 		<Routes>
 			<Route path={ADMIN_ROUTES.DEFAULT.path} element={<AdminOutlet />}>
 				<Route
-					path={ADMIN_ROUTES.DASHBOARD.path}
-					element={<AdminPage />}
+					path={ADMIN_ROUTES.DEFAULT.path}
+					element={<Calendar />}
+				/>
+				<Route path={ADMIN_ROUTES.MASTER.path} element={<Master />} />
+				<Route
+					path={ADMIN_ROUTES.SERVICES.path}
+					element={<Services />}
 				/>
 			</Route>
 		</Routes>
