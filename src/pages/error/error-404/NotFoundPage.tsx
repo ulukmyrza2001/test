35eb77c759b4d742fa6f521549b8ff411c245333
BilimAdisov <112559246+BilimAdisov@users.noreply.github.com>
2023-07-03 17:33:40 +0600

    add route add

--- src/pages/error/error-404/NotFoundPage.tsx
+++ src/pages/error/error-404/NotFoundPage.tsx
@@ -1,35 +1,35 @@
-import Lottie from 'lottie-web'
-import { useEffect, useRef } from 'react'
-import NoPage from '../../../assets/layout-video/NoPage.json'
-import { Button } from '../../../components/UI/Buttons/Button/Button'
-import { useNavigate } from 'react-router'
-import styles from './NotFoundPage.module.css'
+import { useEffect, useRef } from "react";
+import NoPage from "../../../assets/layout-video/NoPage.json";
+import { Button } from "../../../components/UI/Buttons/Button/Button";
+import { useNavigate } from "react-router";
+import styles from "./NotFoundPage.module.css";
+import Lottie from "lottie-web";
 
 export const NotFoundPage = () => {
-	const animateContainer = useRef<any>(null)
-	const navigate = useNavigate()
+  const animateContainer = useRef<any>(null);
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		Lottie.loadAnimation({
-			container: animateContainer.current,
-			renderer: 'svg',
-			loop: true,
-			autoplay: true,
-			animationData: NoPage,
-		})
-	}, [])
+  useEffect(() => {
+    Lottie.loadAnimation({
+      container: animateContainer.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: NoPage,
+    });
+  }, []);
 
-	function handleClick() {
-		navigate('/')
-	}
+  function handleClick() {
+    navigate("/");
+  }
 
-	return (
-		<div className='wrapper-lottie'>
-			<div className={styles.video} ref={animateContainer} />
-			<span className={styles.title}>Код ошибки: 404</span>
-			<Button width='150px' color='white' onClick={() => handleClick()}>
-				Назад
-			</Button>
-		</div>
-	)
-}
+  return (
+    <div className="wrapper-lottie">
+      <div className={styles.video} ref={animateContainer} />
+      <span className={styles.title}>Код ошибки: 404</span>
+      <Button width="150px" color="white" onClick={() => handleClick()}>
+        Назад
+      </Button>
+    </div>
+  );
+};
