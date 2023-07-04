92ef820bda0fdeae46d86215d73a2f5396e6fe82
a6uxa4 <a6uxa4@icloud.com>
2023-07-04 16:25:18 +0600

    saave

--- src/pages/admin/Master/MasterPage/MasterPage.tsx
+++ src/pages/admin/Master/MasterPage/MasterPage.tsx
@@ -1,7 +1,46 @@
+import { Table } from '../../../../components/Tables/Table/Table'
+
 export const Master = () => {
 	return (
-		<div>
-			<div>Master</div>
+		<div style={{ width: '100%' }}>
+			<Table
+				data={[]}
+				columns={[
+					{
+						headerName: '№',
+						field: 'index',
+						flex: 5,
+					},
+					{
+						headerName: 'Категория',
+						field: 'label',
+						flex: 10,
+					},
+					{
+						headerName: '№',
+						field: 'asd',
+						flex: 5,
+					},
+					{
+						headerName: 'Категория',
+						field: 'ga',
+						flex: 10,
+					},
+					{
+						headerName: '№',
+						field: 'ads',
+						flex: 5,
+					},
+					{
+						headerName: 'Категория',
+						field: 'da',
+						flex: 10,
+					},
+				]}
+				loading={false}
+				pagination={true}
+				index={false}
+			/>
 		</div>
 	)
 }
