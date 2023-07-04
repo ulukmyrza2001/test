f00bafe353806ba6c7c85bd1223bde4daf9a1730
Ramazan <ramazanmahmudov03@gmail.com>
2023-07-04 16:48:46 +0600

    accordionUi

--- src/components/UI/Accordion/AccordionUi.tsx
+++ src/components/UI/Accordion/AccordionUi.tsx
@@ -1,156 +1,129 @@
-import React, { useState } from "react";
+import React from "react";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { BsFillQuestionCircleFill } from "react-icons/bs";
 import { BiSolidCircle } from "react-icons/bi";
-import { SiFireship } from "react-icons/si";
 import { AiOutlineRightCircle } from "react-icons/ai";
 import styles from "./Accordion.module.css";
+import { Link } from "react-router-dom";
+
+interface Accordion {
+  id: number;
+  name: string;
+  icon: string;
+  subCategoryServices:
+    | {
+        id: number;
+        name: string;
+        serviceResponses: {
+          id: number;
+          name: string;
+          price: number;
+          duration: {
+            seconds: number;
+            nano: number;
+            negative: boolean;
+            zero: boolean;
+            units:
+              | {
+                  dateBased: boolean;
+                  timeBased: boolean;
+                  durationEstimated: boolean;
+                }[]
+              | [];
+          };
+        };
+      }[]
+    | [];
+}
 
 export const AccordionUi = (props: any) => {
-  const [questionText, setQuestionText] = useState("");
-  const [questionActive, setQuestionActive] = useState(false);
-
-  const handleQuestionClick = (e: React.MouseEvent, text: string) => {
-    e.stopPropagation();
-    setQuestionActive(!questionActive);
-    setQuestionText(text);
-  };
-
   return (
     <>
-      {props.date?.map((el: any) => {
-        return (
-          <div key={el.id}>
-            <Accordion className={styles["MuiPaper-root"]}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <p className={styles.service}>
-                  {React.createElement(el?.icons, {
-                    size: "25",
-                    color: "#7e7e7e",
-                  })}
-                  {el?.salon} ({el?.quantity})
-                </p>
-              </AccordionSummary>
-              <AccordionDetails>
-                {el?.services ? (
-                  el.services.map((item: any) => {
-                    return (
-                      <Accordion
-                        className={styles["MuiPaper-root"]}
-                        key={Math.random()}
-                      >
-                        <AccordionSummary
-                          expandIcon={<ExpandMoreIcon />}
-                          aria-controls="panel1a-content"
-                          id="panel1a-header"
-                        >
-                          <Typography>
-                            {item?.serviceType} ({item?.quantity})
-                          </Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                          {item?.servicesData?.map((elem: any) => {
-                            return (
-                              <div
-                                className={styles.blockItem}
-                                key={Math.random()}
-                              >
-                                <div className={styles.title}>
-                                  <span>
-                                    {elem?.population && (
-                                      <SiFireship color="red" />
-                                    )}
-                                  </span>
-                                  <p>{elem?.title}</p>
-                                  <span>
-                                    {elem?.question && (
-                                      <BsFillQuestionCircleFill
-                                        style={{ cursor: "pointer" }}
-                                        onClick={(e: React.MouseEvent) =>
-                                          handleQuestionClick(e, elem.question)
-                                        }
-                                        color="gray"
-                                      />
-                                    )}
-                                  </span>
-                                  <p className={styles.timePrice}>
-                                    {elem?.price} сом <BiSolidCircle size={8} />{" "}
-                                    {elem?.time}
-                                  </p>
-                                  <hr />
-                                  {questionActive && (
-                                    <p className={styles.questionText}>
-                                      {elem?.question === questionText
-                                        ? questionText
-                                        : ""}
-                                    </p>
-                                  )}
-                                </div>
-                                <span className={styles.iconLink}>
-                                  <AiOutlineRightCircle />
-                                </span>
-                              </div>
-                            );
-                          })}
-                        </AccordionDetails>
-                      </Accordion>
-                    );
-                  })
-                ) : (
-                  <>
-                    {el?.servicesData?.map((elem: any) => {
-                      return (
-                        <div className={styles.blockItem} key={Math.random()}>
-                          <div className={styles.title}>
-                            <span>
-                              {elem?.population && <SiFireship color="red" />}
-                            </span>
-                            <span>{elem?.title}</span>
-                            <span>
-                              {elem?.question && (
-                                <BsFillQuestionCircleFill
-                                  style={{ cursor: "pointer" }}
-                                  onClick={(e: React.MouseEvent) =>
-                                    handleQuestionClick(e, elem.question)
-                                  }
-                                  color="gray"
-                                />
-                              )}
-                            </span>
-                            <span className={styles.timePrice}>
-                              {elem?.price} сом <BiSolidCircle size={8} />{" "}
-                              {elem?.time}
-                            </span>
-                            <hr />
-                            {questionActive && (
-                              <p className={styles.questionText}>
-                                {elem?.question === questionText
-                                  ? questionText
-                                  : ""}
-                              </p>
-                            )}
-                          </div>
-                          <span className={styles.iconLink}>
-                            <AiOutlineRightCircle />
-                          </span>
-                        </div>
-                      );
+      {props.date?.map(
+        (el: {
+          id: number;
+          name: string;
+          icon: string;
+          subCategoryServices: [];
+        }) => {
+          return (
+            <div key={el.id}>
+              <Accordion className={styles["MuiPaper-root"]}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <p className={styles.service}>
+                    {React.createElement(el?.icon, {
+                      size: "25",
+                      color: "#7e7e7e",
                     })}
-                  </>
-                )}
-              </AccordionDetails>
-            </Accordion>
-          </div>
-        );
-      })}
+                    {el?.name}
+                  </p>
+                </AccordionSummary>
+                <AccordionDetails>
+                  {el?.subCategoryServices.map(
+                    (item: {
+                      id: number;
+                      name: string;
+                      serviceResponses: [];
+                    }) => {
+                      return (
+                        <Accordion
+                          className={styles["MuiPaper-root"]}
+                          key={item.id}
+                        >
+                          <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                          >
+                            <Typography>{item?.name}</Typography>
+                          </AccordionSummary>
+                          <AccordionDetails>
+                            {item?.serviceResponses?.map(
+                              (elem: {
+                                id: number;
+                                name: string;
+                                price: number;
+                                duration: { seconds: number; nano: number };
+                              }) => {
+                                return (
+                                  <div
+                                    className={styles.blockItem}
+                                    key={Math.random()}
+                                  >
+                                    <div className={styles.title}>
+                                      <p>{elem?.name}</p>
+                                      <p className={styles.timePrice}>
+                                        {elem?.price} сом{" "}
+                                        <BiSolidCircle size={8} />{" "}
+                                        {elem?.duration.seconds}
+                                        {elem?.duration.nano}
+                                      </p>
+                                    </div>
+                                    <Link to={"#"} className={styles.iconLink}>
+                                      <AiOutlineRightCircle />
+                                    </Link>
+                                  </div>
+                                );
+                              }
+                            )}
+                          </AccordionDetails>
+                        </Accordion>
+                      );
+                    }
+                  )}
+                </AccordionDetails>
+              </Accordion>
+            </div>
+          );
+        }
+      )}
     </>
   );
 };
