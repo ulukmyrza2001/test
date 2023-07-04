df311659c632a11546a8689f9dbdd0bfce5ee110
a6uxa4 <a6uxa4@icloud.com>
2023-07-04 16:27:04 +0600

    save

--- src/pages/admin/Master/MasterPage/MasterPage.tsx
+++ src/pages/admin/Master/MasterPage/MasterPage.tsx
@@ -1,46 +1,7 @@
-import { Table } from '../../../../components/Tables/Table/Table'
-
 export const Master = () => {
 	return (
-		<div style={{ width: '100%' }}>
-			<Table
-				data={[]}
-				columns={[
-					{
-						headerName: '№',
-						field: 'index',
-						flex: 5,
-					},
-					{
-						headerName: 'Категория',
-						field: 'label',
-						flex: 10,
-					},
-					{
-						headerName: '№',
-						field: 'asd',
-						flex: 5,
-					},
-					{
-						headerName: 'Категория',
-						field: 'ga',
-						flex: 10,
-					},
-					{
-						headerName: '№',
-						field: 'ads',
-						flex: 5,
-					},
-					{
-						headerName: 'Категория',
-						field: 'da',
-						flex: 10,
-					},
-				]}
-				loading={false}
-				pagination={true}
-				index={false}
-			/>
+		<div>
+			<div>Master</div>
 		</div>
 	)
 }
