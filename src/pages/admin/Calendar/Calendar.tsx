b1741b8bf759e1cbb3c9025f4621a47d8e7d19cf
a6uxa4 <a6uxa4@icloud.com>
2023-07-05 17:02:58 +0600

    save

--- src/pages/admin/Calendar/Calendar.tsx
+++ src/pages/admin/Calendar/Calendar.tsx
@@ -1,26 +1,53 @@
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCalendar } from '../../../store/features/calendar-slice'
 import { AnyAction } from '@reduxjs/toolkit'
 
+interface CalendarThisDataProps {
+	endStr: string
+	startStr: string
+	view: any
+}
+
 export const Calendar = () => {
 	const { dataCalendar } = useSelector((state: any) => state.calendar)
 
+	const [thisData, setThisData] = useState({
+		startTime: '',
+		endTime: '',
+	})
+
 	const dispatch = useDispatch()
 
-	console.log(dataCalendar)
+	//function
+
+	function handleThisMoment(event: CalendarThisDataProps) {
+		const startDate = new Date(event.startStr)
+		const endDate = new Date(event.endStr)
+
+		setThisData({
+			startTime: startDate.toISOString().split('T')[0],
+			endTime: endDate.toISOString().split('T')[0],
+		})
+	}
+
+	//useEffect
 
 	useEffect(() => {
-		dispatch(
-			getCalendar({
-				startTime: '2023-07-01',
-				endTime: '2023-07-04',
-				masterID: [],
-			}) as unknown as AnyAction,
-		)
-	}, [])
+		if (thisData.startTime === '' || thisData.endTime === '') {
+			console.error('Error no date')
+		} else {
+			dispatch(
+				getCalendar({
+					startTime: thisData.startTime,
+					endTime: thisData.endTime,
+					masterID: [],
+				}) as unknown as AnyAction,
+			)
+		}
+	}, [thisData])
 
 	return (
 		<div style={{ width: '100%' }}>
@@ -28,6 +55,7 @@ export const Calendar = () => {
 				plugins={[dayGridPlugin]}
 				initialView='dayGridMonth'
 				height={700}
+				datesSet={(event) => handleThisMoment(event)}
 			/>
 		</div>
 	)
