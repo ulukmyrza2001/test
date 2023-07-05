e05c9c33f3053b51b3c862abf03eb24557f5aa63
BilimAdisov <112559246+BilimAdisov@users.noreply.github.com>
2023-07-05 17:23:33 +0600

    add slice category select

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { TypedUseSelectorHook, useSelector } from 'react-redux'
-import { authSlice } from './features/auth-slice'
-import { calendarSlice } from './features/calendar-slice'
+import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useSelector } from "react-redux";
+import { authSlice } from "./features/auth-slice";
+import { calendarSlice } from "./features/calendar-slice";
+import { categoryServiceSlice } from "./features/category-service";
 
 export const store = configureStore({
-	reducer: {
-		auth: authSlice.reducer,
-		calendar: calendarSlice.reducer,
-	},
-})
+  reducer: {
+    auth: authSlice.reducer,
+    calendar: calendarSlice.reducer,
+    categoryService: categoryServiceSlice.reducer,
+  },
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
