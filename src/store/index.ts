75a2b71472e4e2f96982456c13ba8dd645dd8f66
a6uxa4 <a6uxa4@icloud.com>
2023-07-06 15:24:39 +0600

    sasve

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,11 @@
-<<<<<<< HEAD
 import { configureStore } from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useSelector } from 'react-redux'
 import { authSlice } from './features/auth-slice'
 import { calendarSlice } from './features/calendar-slice'
 import { companiesSlice } from './features/company-slice'
 import { masterSlice } from './features/master-slice'
+import { categoryServiceSlice } from './features/category-service'
+import { OwnerSlice } from './features/owner-slice'
 
 export const store = configureStore({
 	reducer: {
@@ -12,29 +13,12 @@ export const store = configureStore({
 		calendar: calendarSlice.reducer,
 		master: masterSlice.reducer,
 		companies: companiesSlice.reducer,
+		categoryService: categoryServiceSlice.reducer,
+		owner: OwnerSlice.reducer,
 	},
 })
-=======
-import { configureStore } from "@reduxjs/toolkit";
-import { TypedUseSelectorHook, useSelector } from "react-redux";
-import { authSlice } from "./features/auth-slice";
-import { calendarSlice } from "./features/calendar-slice";
-import { companiesSlice } from "./features/company-slice";
-import { categoryServiceSlice } from "./features/category-service";
-import { OwnerSlice } from "./features/owner-slice";
 
-export const store = configureStore({
-  reducer: {
-    auth: authSlice.reducer,
-    calendar: calendarSlice.reducer,
-    companies: companiesSlice.reducer,
-    categoryService: categoryServiceSlice.reducer,
-    owner: OwnerSlice.reducer,
-  },
-});
->>>>>>> e54d82450274f1b805a325db9f650384eef11933
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
