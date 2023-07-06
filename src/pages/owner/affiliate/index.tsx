e3de776580125e1dcd34336400d3655d0bb009cd
BilimAdisov <112559246+BilimAdisov@users.noreply.github.com>
2023-07-06 18:58:03 +0600

    add modal

--- src/pages/owner/affiliate/index.tsx
+++ src/pages/owner/affiliate/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { InputNumberMask } from "../../../components/UI/Inputs/InputMask/InputMask";
 import styles from "./Announcement.module.css";
+import { ModalComponent } from "../../../components/UI/Modal/Modal";
 
 export const AffiliatePage = () => {
   const [value, setValue] = useState("");
@@ -8,6 +9,19 @@ export const AffiliatePage = () => {
   return (
     <div>
       <InputNumberMask value={value} onChange={(e) => setValue(e)} />
+      <ModalComponent
+        active={true}
+        handleClose={() => console.log("l;")}
+        title="Редактировать котокбаш"
+      >
+        <div style={{ width: "100px" }}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit autem
+            incidunt error maiores voluptatibus voluptatem, veritatis accusamus
+            deserunt aperiam reiciendis?
+          </p>
+        </div>
+      </ModalComponent>
     </div>
   );
 };
