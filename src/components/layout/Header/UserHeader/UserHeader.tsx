416bb739385fbc84c9b6646ce8603efccec53138
hacker <hacker@erkulow.local>
2023-07-08 00:12:31 -0700

    add inner appointmant page

--- src/components/layout/Header/UserHeader/UserHeader.tsx
+++ src/components/layout/Header/UserHeader/UserHeader.tsx
@@ -93,26 +93,10 @@ export const UserHeader = () => {
 		window.location.reload()
 	}
 
-	const [isScrolled, setIsScrolled] = useState(false)
-
-	useEffect(() => {
-		const handleScroll = () => {
-			const scrollTop = window.pageYOffset
-			setIsScrolled(scrollTop > 0)
-		}
-
-		window.addEventListener('scroll', handleScroll)
-		return () => {
-			window.removeEventListener('scroll', handleScroll)
-		}
-	}, [])
-
 	const stylesHcange = () => {
 		if (path === 'barber') {
-			if (isScrolled) {
-				return {
-					background: '#858585',
-				}
+			return {
+				background: '#858585',
 			}
 		} else if (path === 'beauty-salon') {
 			return {
