febacaf9b1c23a1068d60517b5b38a7db3893b00
a6uxa4 <a6uxa4@icloud.com>
2023-07-13 03:31:39 +0600

    save

--- src/components/UI/TimePicker/TimePicker.tsx
+++ src/components/UI/TimePicker/TimePicker.tsx
@@ -22,13 +22,20 @@ export default function BasicTimePicker({
 	minutesStep,
 	disabled,
 }: BasixTimePickerProps) {
+	const today = new Date()
+	const [hours, minutes, seconds] = value.split(':')
+
+	today.setHours(parseInt(hours))
+	today.setMinutes(parseInt(minutes))
+	today.setSeconds(parseInt(seconds))
+
 	return (
 		<LocalizationProvider dateAdapter={AdapterDayjs}>
 			<DemoContainer
 				components={['TimePicker']}
 				sx={{ overflow: 'initial', paddingTop: 0 }}>
 				<StyledTimePicker
-					value={value}
+					value={dayjs(today)}
 					onChange={(time: any) => {
 						const formattedTime = dayjs(time).format('HH:mm:ss')
 						onChange(formattedTime)
--- src/pages/admin/Master/MasterInnerPage/Schedule/AddFullSchedule/AddFullSchedule.tsx
+++ src/pages/admin/Master/MasterInnerPage/Schedule/AddFullSchedule/AddFullSchedule.tsx
@@ -1,9 +1,123 @@
+import { useState } from 'react'
+import { DataPicker } from '../../../../../../components/UI/DataPicker/DataPicker'
+import { ModalComponent } from '../../../../../../components/UI/Modal/Modal'
+import { FULL_WEEK } from '../../../../../../utils/constants/constants'
 import styles from './AddFullSchedule.module.css'
+import dayjs from 'dayjs'
+import BasicTimePicker from '../../../../../../components/UI/TimePicker/TimePicker'
+import { Switch } from '../../../../../../components/UI/Switch/Switch'
+import { Button } from '../../../../../../components/UI/Buttons/Button/Button'
 
 export const AddFullSchedule = () => {
+	const [scheduleData, setScheduleData] = useState({
+		startDate: dayjs(new Date()).format('YYYY-MM-DD'),
+		endDate: '',
+		dayScheduleRequests: [
+			{
+				startTime: '',
+				endTime: '',
+				dayOfWeek: 'MONDAY',
+				workingDay: false,
+			},
+			{
+				startTime: '',
+				endTime: '',
+				dayOfWeek: 'TUESDAY',
+				workingDay: false,
+			},
+			{
+				startTime: '',
+				endTime: '',
+				dayOfWeek: 'WEDNESDAY',
+				workingDay: false,
+			},
+			{
+				startTime: '',
+				endTime: '',
+				dayOfWeek: 'THURSDAY',
+				workingDay: false,
+			},
+			{
+				startTime: '',
+				endTime: '',
+				dayOfWeek: 'FRIDAY',
+				workingDay: false,
+			},
+			{
+				startTime: '',
+				endTime: '',
+				dayOfWeek: 'SATURDAY',
+				workingDay: false,
+			},
+			{
+				startTime: '',
+				endTime: '',
+				dayOfWeek: 'SUNDAY',
+				workingDay: false,
+			},
+		],
+	})
+
+	// console.log(scheduleData)
 	return (
-		<div>
-			<div>AddFullSchedule</div>
-		</div>
+		<ModalComponent
+			active={true}
+			handleClose={() => false}
+			title='Создать график'>
+			<div className={styles.container_full_schedule}>
+				<div className={styles.container_full_schedule_header}>
+					<DataPicker
+						value={scheduleData.startDate}
+						onChange={(e: string) =>
+							setScheduleData({
+								...scheduleData,
+								startDate: e,
+							})
+						}
+					/>
+					<DataPicker
+						value={scheduleData.endDate}
+						onChange={(e: string) =>
+							setScheduleData({
+								...scheduleData,
+								endDate: e,
+							})
+						}
+					/>
+				</div>
+				{FULL_WEEK.map((item: { nameEN: string; nameRU: string }) => {
+					return (
+						<div
+							key={item.nameRU}
+							className={styles.card_full_schedule}>
+							<Switch
+								checked={true}
+								onChange={(e) => console.log(e)}
+							/>
+							<span>{item.nameRU}</span>
+							<BasicTimePicker
+								value={''}
+								onChange={(e) => console.log(e)}
+								minHours={0}
+								maxHours={23}
+								minutesStep={30}
+							/>
+							<BasicTimePicker
+								value={''}
+								onChange={(e) => console.log(e)}
+								minHours={0}
+								maxHours={23}
+								minutesStep={30}
+							/>
+						</div>
+					)
+				})}
+				<div className={styles.container_full_schedule_footer}>
+					<Button>Отмена</Button>
+					<Button>Применить к всем</Button>
+					<Button>Сохранить</Button>
+				</div>
+			</div>
+		</ModalComponent>
 	)
 }
