5acbb6d7ee5242d13401dbc81f79e47c022b5c6f
hacker <hacker@erkulow.local>
2023-07-11 17:19:27 -0700

    sasve

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -25,45 +25,43 @@ export const Banner = () => {
 	}
 
 	return (
-		<Container>
-			<div className={styles.wrapper}>
-				<div className={styles.block_1}>
-					<h2 className='title' style={{ fontSize: '30px' }}>
-						Сервис онлайн бронирования в сфере красоты и здоровья.
-					</h2>
-					<p>Сегодня 7639 женщин меняют прическу</p>
-				</div>
-				<div className={styles.block_2}>
-					<img src={BannerIMG} alt='banner' />
-				</div>
-				<div className={styles.wrapper_input}>
-					<div className={styles.input}>
-						<div
-							className={styles.search_input}
-							onClick={showSearchModalHandler}
-						>
-							<HiOutlineMagnifyingGlass />
-							<span>Book your services...</span>
-						</div>
-						<SearchModal
-							active={showSearchModal}
-							hideHandler={hideSearchModalHandler}
-						/>
-						<GeoModal
-							active={showGeoModal}
-							hideHandler={hideGeoModalHandler}
-						/>
-						<hr />
-						<div
-							className={styles.search_input}
-							onClick={showGeoModalHandler}
-						>
-							<BsGeoAlt />
-							<span>Bishkek, Baitik Batyr Street</span>
-						</div>
+		<div className={styles.wrapper}>
+			<div className={styles.wrapper_input}>
+				<div className={styles.input}>
+					<div
+						className={styles.search_input}
+						onClick={showSearchModalHandler}
+					>
+						<HiOutlineMagnifyingGlass fontSize={20} />
+						<span>Book your services...</span>
+					</div>
+					<SearchModal
+						active={showSearchModal}
+						hideHandler={hideSearchModalHandler}
+					/>
+					<GeoModal
+						active={showGeoModal}
+						hideHandler={hideGeoModalHandler}
+					/>
+					<hr />
+					<div
+						className={styles.search_input}
+						onClick={showGeoModalHandler}
+					>
+						<BsGeoAlt fontSize={20} />
+						<span>Bishkek, Baitik Batyr Street</span>
 					</div>
 				</div>
 			</div>
-		</Container>
+			<div className={styles.block_1}>
+				<h2 className='title' style={{ fontSize: '30px' }}>
+					Сервис онлайн бронирования в сфере красоты и здоровья.
+				</h2>
+				<p>Сегодня 7639 женщин меняют прическу</p>
+			</div>
+			<div className={styles.block_2}>
+				<img src={BannerIMG} alt='banner' />
+			</div>
+		</div>
 	)
 }
