eb6c4a34407085be712cf716c2829e8f2af6e724
a6uxa4 <a6uxa4@icloud.com>
2023-07-15 10:26:40 +0600

    save

--- src/pages/admin/Calendar/Calendar.tsx
+++ src/pages/admin/Calendar/Calendar.tsx
@@ -1,11 +1,19 @@
 import FullCalendar from '@fullcalendar/react'
+import listPlugin from '@fullcalendar/list'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import { FormatterInput } from '@fullcalendar/core'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
 import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCalendar } from '../../../store/features/calendar-slice'
 import { AnyAction } from '@reduxjs/toolkit'
 import { Backdrop, CircularProgress } from '@mui/material'
+import {
+	calendarTimeFormat,
+	headerToolbar,
+	translatebuttonText,
+} from './contants'
 
 interface CalendarThisDataProps {
 	endStr: string
@@ -25,11 +33,6 @@ export const Calendar = () => {
 	const dispatch = useDispatch()
 
 	//const
-
-	const calendarTimeFormat = {
-		hour: 'numeric',
-		minute: '2-digit',
-	}
 
 	const isLoadingSx = {
 		color: '#fff',
@@ -70,18 +73,41 @@ export const Calendar = () => {
 
 	return (
 		<div style={{ width: '100%' }}>
+			<div></div>
 			<Backdrop sx={isLoadingSx} open={globalLoading}>
 				<CircularProgress color='inherit' />
 			</Backdrop>
 			<FullCalendar
-				plugins={[dayGridPlugin]}
-				initialView='dayGridMonth'
+				plugins={[
+					dayGridPlugin,
+					timeGridPlugin,
+					interactionPlugin,
+					listPlugin,
+				]}
 				locale='ru'
 				firstDay={1}
-				height={700}
+				height={'89vh'}
+				eventMaxStack={1}
+				slotMinTime={'09:00'}
+				slotMaxTime={'23:00'}
+				initialView='dayGridMonth'
+				noEventsText='Нет Записей'
+				headerToolbar={headerToolbar}
+				buttonText={translatebuttonText}
+				moreLinkContent={(el) => `${el.shortText} Еще`}
 				eventTimeFormat={calendarTimeFormat as FormatterInput}
+				selectable={true}
+				allDaySlot={false}
+				dayMaxEvents={true}
+				fixedWeekCount={false}
+				eventStartEditable={true}
+				eventDurationEditable={true}
 				datesSet={(event) => handleThisMoment(event)}
-				events={dataCalendar.map((item: any) => ({
+				select={(w) => console.log(w, 'select')}
+				eventDrop={(e: any) => console.log(e, 'eventDrop')}
+				dateClick={(e: any) => console.log(e, 'dateClick')}
+				eventClick={(w: any) => console.log(w, 'eventClick')}
+				events={dataCalendar?.map((item: any) => ({
 					start: item.startTime,
 					end: item.endTime,
 					title: `${item.masterFirstName}\u00A0${item.masterLastName}`,
---
+++ src/pages/admin/Calendar/contants.ts
@@ -0,0 +1,18 @@
+export const calendarTimeFormat = {
+	hour: 'numeric',
+	minute: '2-digit',
+}
+
+export const headerToolbar = {
+	left: 'prev,dayGridMonth,timeGridWeek',
+	center: 'title',
+	right: 'today,timeGridDay,listWeek,next',
+}
+
+export const translatebuttonText = {
+	today: 'Сегодня',
+	day: 'День',
+	month: 'Месяц',
+	week: 'Неделя',
+	list: 'Список',
+}
