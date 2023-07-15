bf90fda97c76fa69c190b3e4ed2e2d4bec522ab8
a6uxa4 <a6uxa4@icloud.com>
2023-07-16 04:33:43 +0600

    save

--- src/components/UI/Selects/LonelySelect/LonelySelect.tsx
+++ src/components/UI/Selects/LonelySelect/LonelySelect.tsx
@@ -1,44 +1,51 @@
-import React from "react";
-import Select from "react-select";
-import styles from "./LonelySelect.module.css";
+import React from 'react'
+import Select from 'react-select'
+import styles from './LonelySelect.module.css'
 
 interface LonelySelectProps {
-  options: { label: string; value: string | number }[] | [];
-  label: string;
-  placeholder: string;
-  noOptionsMessage: (obj: { inputValue: string }) => React.ReactNode;
-  value: { label: string; value: string | number } | null;
-  onChange: (e: any) => void;
-  isClearable: boolean;
-  isLoading: boolean;
-  isDisabled: boolean;
+	options: { label: string; value: string | number }[] | []
+	label: string
+	placeholder: string
+	noOptionsMessage: (obj: { inputValue: string }) => React.ReactNode
+	value: { label: string; value: string | number } | null
+	onChange: (e: any) => void
+	isClearable: boolean
+	isLoading: boolean
+	isDisabled: boolean
 }
 
 export const LonelySelect = ({
-  options = [],
-  label = "",
-  placeholder = "",
-  noOptionsMessage = () => null,
-  value = null,
-  onChange = () => {},
-  isClearable = false,
-  isLoading = false,
-  isDisabled = false,
+	options = [],
+	label = '',
+	placeholder = '',
+	noOptionsMessage = () => null,
+	value = null,
+	onChange = () => {},
+	isClearable = false,
+	isLoading = false,
+	isDisabled = false,
 }: LonelySelectProps) => {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.label}>{label}</div>
-      <Select
-        options={options}
-        placeholder={placeholder}
-        noOptionsMessage={noOptionsMessage}
-        value={value}
-        onChange={onChange}
-        isClearable={isClearable}
-        isLoading={isLoading}
-        isDisabled={isLoading || isDisabled}
-        isSearchable={true}
-      />
-    </div>
-  );
-};
+	const customStyles = {
+		control: (provided: any, state: any) => ({
+			...provided,
+			height: '40px',
+		}),
+	}
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.label}>{label}</div>
+			<Select
+				styles={customStyles}
+				options={options}
+				placeholder={placeholder}
+				noOptionsMessage={noOptionsMessage}
+				value={value}
+				onChange={onChange}
+				isClearable={isClearable}
+				isLoading={isLoading}
+				isDisabled={isLoading || isDisabled}
+				isSearchable={true}
+			/>
+		</div>
+	)
+}
--- src/pages/admin/Calendar/AddAppoinmentsModal/AddAppoinmentsModal.tsx
+++ src/pages/admin/Calendar/AddAppoinmentsModal/AddAppoinmentsModal.tsx
@@ -1,5 +1,8 @@
 import styles from './AddAppoinments.module.css'
 import { ModalComponent } from '../../../../components/UI/Modal/Modal'
+import { Input } from '../../../../components/UI/Inputs/Input/Input'
+import { MultiSelect } from '../../../../components/UI/Selects/MultiSelect/MultiSelect'
+import { DataPicker } from '../../../../components/UI/DataPicker/DataPicker'
 
 interface AddAppoinmentsModalProps {
 	active: boolean
@@ -24,7 +27,37 @@ export const AddAppoinmentsModal = ({
 			active={active}
 			handleClose={handleClose}
 			title='Новый визит'>
-			<div>IN PROGRESS</div>
+			<div className={styles.container_appoinment}>
+				<div className={styles.container}>
+					<div className={styles.wrapper}>
+						<Input label='Мастер' placeholder='Мастер' />
+					</div>
+					<div className={styles.wrapper}>
+						<MultiSelect
+							label='Услуги'
+							options={[]}
+							placeholder={'Услуги'}
+							noOptionsMessage={() => 'Нет услуги'}
+							value={[]}
+							onChange={(e: any) => console.log(e)}
+							isLoading={false}
+							isClearable={true}
+							isDisabled={false}
+						/>
+					</div>
+				</div>
+				<div className={styles.container}>
+					<div className={styles.wrapper}>
+						<DataPicker value={''} onChange={() => ''} />
+					</div>
+					<div className={styles.wrapper}>
+						<span>Hello</span>
+					</div>
+					<div className={styles.wrapper}>
+						<span>Hello</span>
+					</div>
+				</div>
+			</div>
 		</ModalComponent>
 	)
 }
