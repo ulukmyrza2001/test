1704074023ee3be901ed795f45ddcbc9a84184dd
a6uxa4 <a6uxa4@icloud.com>
2023-07-16 23:10:11 +0600

    save ;

--- src/pages/admin/Master/MasterInnerPage/Schedule/Schedule.tsx
+++ src/pages/admin/Master/MasterInnerPage/Schedule/Schedule.tsx
@@ -96,8 +96,7 @@ export const Schedule = ({ startWeek }: ScheduleProps) => {
 						return (
 							<div
 								className={styles.container_name_week}
-								key={el}
-							>
+								key={el}>
 								<h1>{el}:</h1>
 								<Skeleton
 									variant='rectangular'
@@ -113,8 +112,7 @@ export const Schedule = ({ startWeek }: ScheduleProps) => {
 						return (
 							<div
 								className={styles.container_name_week}
-								key={element}
-							>
+								key={element}>
 								<h1>{element}:</h1>
 								<div className={styles.container_week}>
 									<div></div>
@@ -129,12 +127,10 @@ export const Schedule = ({ startWeek }: ScheduleProps) => {
 							).getTime()
 							const percentage =
 								((endTime / maxEndTime) * 100).toFixed(2) + '%'
-
 							return (
 								<div
 									className={styles.container_name_week}
-									key={item.dayScheduleId}
-								>
+									key={item.dayScheduleId}>
 									<h1>{TranslateWeekShort(item.week)}:</h1>
 									<div className={styles.container_week}>
 										<div
@@ -148,8 +144,7 @@ export const Schedule = ({ startWeek }: ScheduleProps) => {
 												opacity: item.workingDay
 													? '1'
 													: '0',
-											}}
-										>
+											}}>
 											{`${item.startTime.slice(
 												0,
 												5,
