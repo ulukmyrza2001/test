75a69bc95c27320ccb6dd5f0935f3f91ace66646
hacker <hacker@erkulow.local>
2023-07-20 11:19:41 +0600

    save

--- src/pages/user/beauty-salon/popular-services/index.tsx
+++ src/pages/user/beauty-salon/popular-services/index.tsx
@@ -1,7 +1,49 @@
 import React from 'react'
 import styles from './PopularServices.module.css'
 import { Container } from '../../../../styles/ContainerStyle/Container'
+import Test from '../../../../assets/image/imgBarber.png'
+
+const data = [
+	{
+		img: Test,
+		name: 'brows and lashes',
+	},
+	{
+		img: Test,
+		name: 'brows and lashes',
+	},
+	{
+		img: Test,
+		name: 'brows and lashes',
+	},
+	{
+		img: Test,
+		name: 'brows and lashes',
+	},
+	{
+		img: Test,
+		name: 'brows and lashes',
+	},
+	{
+		img: Test,
+		name: 'brows and lashes',
+	},
+]
 
 export const PopularServicesContent = () => {
-	return <Container>PopularServicesContent</Container>
+	return (
+		<Container>
+			<div className={styles.wrapper}>
+				<h1 className={styles.title}>• Популярные Услуги •</h1>
+				<div className={styles.wrapper_cards}>
+					{data?.map((item: any) => (
+						<div className={styles.card}>
+							<img src={item.img} alt='logo' />
+							<div className={styles.name}>{item.name}</div>
+						</div>
+					))}
+				</div>
+			</div>
+		</Container>
+	)
 }
